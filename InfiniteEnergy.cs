a3255153d8290d7e72c3919675f349d9fca02939
Jdance <Jdance@DESKTOP-H0JESVK>
2022-02-09 16:40:15 -0500

    Add project files.

---
+++ InfiniteEnergy.cs
@@ -0,0 +1,31 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiniteEnergy
+{
+    public class InfiniteEnergy : RocketPlugin
+    {
+        protected override void Load()
+        {
+            UnturnedPlayerEvents.OnPlayerUpdateStamina += UnturnedPlayerEvents_OnPlayerUpdateStamina;
+        }
+
+        private void UnturnedPlayerEvents_OnPlayerUpdateStamina(Rocket.Unturned.Player.UnturnedPlayer player, byte stamina)
+        {
+            if (stamina <= 50)
+            {
+                player.Player.life.serverModifyStamina(100);
+            }
+        }
+
+        protected override void Unload()
+        {
+            
+        }
+    }
+}
