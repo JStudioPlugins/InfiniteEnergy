0f9334158de096a3ede1696f997c70e3becdf38b
Jdance-Media <67397422+Jdance-Media@users.noreply.github.com>
2023-05-20 22:04:04 -0400

    Restore UI changes, made code more concise.

--- InfiniteEnergy.cs
+++ InfiniteEnergy.cs
@@ -1,5 +1,8 @@
-﻿using Rocket.Core.Plugins;
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned;
 using Rocket.Unturned.Events;
+using SDG.Unturned;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,20 +15,26 @@ namespace InfiniteEnergy
     {
         protected override void Load()
         {
-            UnturnedPlayerEvents.OnPlayerUpdateStamina += UnturnedPlayerEvents_OnPlayerUpdateStamina;
+            UnturnedPlayerEvents.OnPlayerUpdateStamina += HandlePlayerStaminaUpdate;
+            U.Events.OnPlayerConnected += HandlePlayerConnected;
+            Logger.Log("InfiniteEnergy by JStudio has now been loaded");
         }
-
-        private void UnturnedPlayerEvents_OnPlayerUpdateStamina(Rocket.Unturned.Player.UnturnedPlayer player, byte stamina)
+        private void HandlePlayerConnected(Rocket.Unturned.Player.UnturnedPlayer player)
+        {
+            player.Player.disablePluginWidgetFlag(EPluginWidgetFlags.ShowStamina);
+        }
+        private void HandlePlayerStaminaUpdate(Rocket.Unturned.Player.UnturnedPlayer player, byte stamina)
         {
             if (stamina <= 50)
-            {
                 player.Player.life.serverModifyStamina(100);
-            }
         }
-
         protected override void Unload()
         {
-            
+            UnturnedPlayerEvents.OnPlayerUpdateStamina -= HandlePlayerStaminaUpdate;
+            U.Events.OnPlayerConnected -= HandlePlayerConnected;
+            foreach (SteamPlayer player in Provider.clients)
+                player.player.enablePluginWidgetFlag(EPluginWidgetFlags.ShowStamina);
+            Logger.Log("InfiniteEnergy by JStudio has now been unloaded. UI changes restored.");
         }
     }
 }
